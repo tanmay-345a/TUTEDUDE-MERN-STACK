ef29897325aa4d9378798f3a7763f5895b63ca73
Tanmay Shakya <tanmayshakya2210@gmail.com>
2026-09-14 14:19:31 +0530

    Add files via upload

---
+++ TASK-13/script.js
@@ -0,0 +1,44 @@
+// Check whether given number is even or odd.
+
+const num1 = 3;
+const num2 = 5;
+const num3 = 6;
+const num4 = 7;
+const num5 = 9;
+const num6 = 10;
+
+if(num1%2 == 0){
+    console.log("The number",num1 ,"is even.");
+}else{
+    console.log("The number",num1 ,"is odd.");
+}
+
+if(num2%2 == 0){
+    console.log("The number",num2 ,"is even.");
+}else{
+    console.log("The number",num2 ,"is odd.");
+}
+
+if(num3%2 == 0){
+    console.log("The number",num3 ,"is even.");
+}else{
+    console.log("The number",num3 ,"is odd.");
+}
+
+if(num4%2 == 0){
+    console.log("The number",num4 ,"is even.");
+}else{
+    console.log("The number",num4 ,"is odd.");
+}
+
+if(num5%2 == 0){
+    console.log("The number",num5 ,"is even.");
+}else{
+    console.log("The number",num5 ,"is odd.");
+}
+
+if(num6%2 == 0){
+    console.log("The number",num6 ,"is even.");
+}else{
+    console.log("The number",num6 ,"is odd.");
+}
